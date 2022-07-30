b737ecf5a11852cace0631563142c81b2007b21b
Antonio Scognamiglio <a.scognamiglio32@studenti.unisa.it>
2022-07-30 16:22:55 +0200

    refactor: cleanup code

--- cypress/e2e/testfolder/index.cy.ts
+++ cypress/e2e/testfolder/index.cy.ts
@@ -1,33 +1,31 @@
 const arrayWords = [
-  "AGGIUNGI AL CARRELLO",
-  "AGGIUNGI AL BASKET",
-  "AGGIUNGI",
-  "ADD TO CART",
+    "AGGIUNGI AL CARRELLO",
+    "AGGIUNGI AL BASKET",
+    "AGGIUNGI",
+    "ADD TO CART"
 ];
 
-it("Sneak Into Basket", () => {
-  cy.visit("https://shop.pastagarofalo.it/it/mix-farine-7kg/");
-  cy.wait(2000);
-  cy.get("button")
-    .then((e) => {
-      Cypress.$(e).each((i, element) => {
-        arrayWords.forEach((word) => {
-          if (Cypress.$(element).text().toUpperCase().includes(word)) {
-            cy.wrap(element).click({ force: true });
-          }
-        });
-      });
-    })
-    .wait(1000);
-  cy.visit("https://shop.pastagarofalo.it/it/cart/")
-    .wait(2000)
-    .screenshot("screenshot", { capture: "viewport" });
-});
+const PRODUCT_URL = "https://shop.pastagarofalo.it/it/mix-farine-7kg/";
+const CART_URL = "https://shop.pastagarofalo.it/it/cart/";
+const WAIT_TIME = 800;
 
-/**
- * 1. Input pagina prodotto e pagina carrello
- * 2. Cypress aggiungere 1 prodotto al carrello
- * 3. Navigare pagina carrello
- * 4. Screenshot pagina carrello
- * 5. Tensorflow classificazione
- */
+it("Sneak Into Basket", () => {
+    cy.visit(PRODUCT_URL)
+        .wait(WAIT_TIME)
+        .get("button")
+        .then((e) => {
+            Cypress.$(e).each((_, element) => {
+                arrayWords.forEach((word) => {
+                    if (
+                        Cypress.$(element).text().toUpperCase().includes(word)
+                    ) {
+                        cy.wrap(element).click({ force: true });
+                    }
+                });
+            });
+        })
+        .wait(700)
+        .visit(CART_URL)
+        .wait(WAIT_TIME)
+        .screenshot("screenshot", { capture: "viewport" });
+});
--- src/index.ts
+++ src/index.ts
@@ -5,36 +5,57 @@ import cypress from "cypress";
 import * as puppeteer from "puppeteer";
 
 (async () => {
-  await cypress.run({
-    spec: path.join("cypress", "e2e", "testfolder", "index.cy.ts"),
-    browser: "chrome",
-  });
+    const launchHttpServer = () => {
+        const app = express();
+        const port = 3000;
+        app.use(cors());
+        app.get("/", (_, res) => {
+            res.sendFile("screenshot.png", {
+                root: path.join(
+                    __dirname,
+                    "..",
+                    "..",
+                    "cypress",
+                    "screenshots",
+                    "index.cy.ts"
+                )
+            });
+        });
 
-  const app = express();
-  const port = 3000;
-  app.use(cors());
+        app.get("/send", (req, res) => {
+            const { p1, p2 } = req.query;
+            res.send("OK");
+            console.log("1Prodotto: ", p1);
+            console.log("2Prodotti: ", p2);
+            process.exit();
+        });
+        app.listen(port, async () => {
+            console.log(`Server running on port ${port}`);
+        });
+    };
 
-  app.get("/", (req, res) => {
-    res.sendFile("screenshot.png", {
-      root: __dirname + "..\\..\\..\\cypress\\screenshots\\index.cy.ts",
-    });
-  });
+    const simulateInteraction = async () => {
+        await cypress
+            .run({
+                spec: path.join("cypress", "e2e", "testfolder", "index.cy.ts"),
+                browser: "chrome",
+                config: {
+                    video: false
+                }
+            })
+            .catch((e) => {
+                console.error("Errore nell'interazione con la pagina");
+                console.error(e);
+            });
+    };
 
-  app.get("/send", (req, res) => {
-    const { p1, p2 } = req.query;
-    res.send("OK");
-    console.log("1Prodotto: ", p1);
-    console.log("2Prodotti: ", p2);
-    process.exit();
-  });
+    const classify = async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto("file:///" + process.cwd() + "/index.html");
+    };
 
-  app.listen(port, async () => {
-    console.log(`Server running on port ${port}`);
-
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("file:///" + process.cwd() + "/index.html");
-  });
+    launchHttpServer();
+    await simulateInteraction();
+    await classify();
 })();
-
-console.log("file:///" + process.cwd() + "/index.html");
