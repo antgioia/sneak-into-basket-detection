346a1f18e9ca34124d046a5a5e40f2d372c92998
antgioia <antgioia10@gmail.com>
2022-08-12 13:37:23 +0200

    feat: modify cypress. file

--- cypress/e2e/testfolder/index.cy.ts
+++ cypress/e2e/testfolder/index.cy.ts
@@ -1,8 +1,9 @@
 const arrayWords = [
-    "AGGIUNGI AL CARRELLO",
-    "AGGIUNGI AL BASKET",
-    "AGGIUNGI",
-    "ADD TO CART"
+  "AGGIUNGI AL CARRELLO",
+  "AGGIUNGI AL BASKET",
+  "AGGIUNGI",
+  "ADD TO CART",
+  "ADD TO MY CART",
 ];
 
 const PRODUCT_URL = "https://shop.pastagarofalo.it/it/mix-farine-7kg/";
@@ -10,22 +11,20 @@ const CART_URL = "https://shop.pastagarofalo.it/it/cart/";
 const WAIT_TIME = 800;
 
 it("Sneak Into Basket", () => {
-    cy.visit(PRODUCT_URL)
-        .wait(WAIT_TIME)
-        .get("button")
-        .then((e) => {
-            Cypress.$(e).each((_, element) => {
-                arrayWords.forEach((word) => {
-                    if (
-                        Cypress.$(element).text().toUpperCase().includes(word)
-                    ) {
-                        cy.wrap(element).click({ force: true });
-                    }
-                });
-            });
-        })
-        .wait(700)
-        .visit(CART_URL)
-        .wait(WAIT_TIME)
-        .screenshot("screenshot", { capture: "viewport" });
+  cy.visit(PRODUCT_URL)
+    .wait(WAIT_TIME)
+    .get("button")
+    .then((e) => {
+      Cypress.$(e).each((_, element) => {
+        arrayWords.forEach((word) => {
+          if (Cypress.$(element).text().toUpperCase().includes(word)) {
+            cy.wrap(element).click({ force: true });
+          }
+        });
+      });
+    })
+    .wait(700)
+    .visit(CART_URL)
+    .wait(WAIT_TIME)
+    .screenshot("screenshot", { capture: "viewport" });
 });
